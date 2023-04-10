e65c0a2a3dde5f69d009a25d1bd642486c4db380
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-10 16:42:38 +0300

    add task pk

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, TaskDetails
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetails.as_view(), name='task'),
+    
     
 ]
