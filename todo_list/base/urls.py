9119d4454bb39ea8a8d11e45d19f34789c9373e1
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-10 15:32:49 +0300

    fix bug

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import TaskList
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='task'),
+    # path('', TaskList.as_view(), name='tasks'),
+    
 ]
