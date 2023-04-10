cdf1164a8fd57a5f958089866383c6cf89f43a8c
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-10 15:56:56 +0300

    create task list

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TaskList
 
 urlpatterns = [
-    # path('', TaskList.as_view(), name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
     
 ]
--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'.П
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
