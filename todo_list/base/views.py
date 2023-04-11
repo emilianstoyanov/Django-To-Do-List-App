db1bdb23075eeb495de5892041ca179df94417d6
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-11 13:50:36 +0300

    create class task create

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
+from django.views.generic.edit import CreateView
 from .models import Task
 # Create your views here.
 
@@ -14,3 +14,6 @@ class TaskDetails(DetailView):
     context_object_name = 'task'
     template_name = 'base/task.html'
     
+class TaskCreate(CreateView):
+    model = Task
+    field = '__all__'
