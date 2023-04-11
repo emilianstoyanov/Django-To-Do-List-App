b2a54d61a9bf729e61c4ecf6e56fed21d434d1f8
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-11 15:48:19 +0300

    logout functionality

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import TaskList, TaskDetails, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
 
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path("logout/", LogoutView.as_view(next_page='login'), name="logout"),
+    
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetails.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
