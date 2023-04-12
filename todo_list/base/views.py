6d91635243125c0b2a5d851c7ba3f9916858a402
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-12 09:55:31 +0300

    only returns objects that belongs to the current user

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -6,11 +6,19 @@ from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.contrib.auth.mixins import OwnObjectsMixin
 
 from .models import Task
 # Create your views here.
 
+class OwnObjectsMixin():
+ """
+ Only returns objects that belongs to the current user. Assumes the object
+ has a 'user' foreignkey to a User.
+ """
+ def get_queryset(self):
+    user = self.request.user
+    return super(OwnObjectsMixin, self).get_queryset().filter(user=user)
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
@@ -19,29 +27,30 @@ class CustomLoginView(LoginView):
     
     def get_success_url(self):
         return reverse_lazy('tasks')
+        
 
 
-class TaskList(LoginRequiredMixin, ListView):
+class TaskList(OwnObjectsMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
-class TaskDetails(LoginRequiredMixin, DetailView):
+class TaskDetails(OwnObjectsMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
     
-class TaskCreate(LoginRequiredMixin, CreateView):
+class TaskCreate(OwnObjectsMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
     
-class TaskUpdate(LoginRequiredMixin, UpdateView):
+class TaskUpdate(OwnObjectsMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
     
 
-class TaskDelete(LoginRequiredMixin, DeleteView):
+class TaskDelete(OwnObjectsMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
