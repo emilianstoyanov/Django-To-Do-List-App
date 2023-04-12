0a19fff6fa8ffe2075e8de9cc2f355ab22e8dad0
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-12 14:49:30 +0300

    fix register form

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
@@ -18,28 +18,38 @@ class OwnObjectsMixin():
  """
  def get_queryset(self):
     user = self.request.user
-    return super(OwnObjectsMixin, self).get_queryset().filter(user=user)
+    return super(OwnObjectsMixin, self).get_queryset().filter(user=user.id)
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
-    redirect_authenticated_user = True	
-    
+    redirect_authenticated_user = True
+
     def get_success_url(self):
         return reverse_lazy('tasks')
-        
+
+
 class RegisterPage(FormView):
     template_name = 'base/register.html'
     form_class = UserCreationForm
     redirect_authenticated_user = True
     success_url = reverse_lazy('tasks')
 
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
     
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
 
 class TaskList(OwnObjectsMixin, ListView):
     model = Task
     context_object_name = 'tasks'
-    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
@@ -57,7 +67,7 @@ class TaskCreate(OwnObjectsMixin, CreateView):
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
     
-    def form_invalid(self, form):
+    def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
     
